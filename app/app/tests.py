412914add172b5ad169e74f3d21bc36c42ab1e1f
IT Santa Ursula Bandung <it@santaursula-bdg.sch.id>
2024-02-25 17:47:25 +0700

    Write Sample Test

---
+++ app/app/tests.py
@@ -0,0 +1,12 @@
+"""
+Sample Test
+"""
+
+from django.test import SimpleTestCase
+from app import calc
+
+
+class CalcTests(SimpleTestCase):
+    def test_add_number(self):
+        res = calc.add(5, 6)
+        self.assertEqual(res, 11)
