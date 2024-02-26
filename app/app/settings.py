883a79b6a30d268d4ad36910c5fa100013bba630
IT Santa Ursula Bandung <it@santaursula-bdg.sch.id>
2024-02-26 15:57:46 +0700

    Test Swagger UI

--- app/app/settings.py
+++ app/app/settings.py
@@ -132,5 +132,5 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 AUTH_USER_MODEL = 'core.User'
 
 REST_FRAMEWORK = {
-    'DEFAULT_AUTHENTICATION_CLASSES': 'drf_spectacular.openapi.AutoSchema',
+    'DEFAULT_SCHEMA_CLASS': 'drf_spectacular.openapi.AutoSchema',
 }
