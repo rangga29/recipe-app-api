29fb6140dbd990f19e33d9fcc2881e61cfdeeaa8
IT Santa Ursula Bandung <it@santaursula-bdg.sch.id>
2024-02-26 14:24:39 +0700

    Support Modifying Users

--- app/core/tests/test_admin.py
+++ app/core/tests/test_admin.py
@@ -26,5 +26,11 @@ class AdminSiteTests(TestCase):
         url = reverse('admin:core_user_changelist')
         res = self.client.get(url)
 
-        self.assertEqual(res, self.admin_user.name)
-        self.assertEqual(res, self.admin_user.email)
+        self.assertContains(res, self.user.name)
+        self.assertContains(res, self.user.email)
+
+    def test_edit_user(self):
+        url = reverse('admin:core_user_change', args=[self.user.id])
+        res = self.client.get(url)
+
+        self.assertEqual(res.status_code, 200)
