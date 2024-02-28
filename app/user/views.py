dbc243f7aa3255f5e3fb4072dd8fa8ba11694f29
IT Santa Ursula Bandung <it@santaursula-bdg.sch.id>
2024-02-28 13:22:07 +0700

    Implement Manage User API (Error)

--- app/user/views.py
+++ app/user/views.py
@@ -2,7 +2,7 @@
 Views for User API
 """
 
-from rest_framework import generics
+from rest_framework import generics, authentication, permissions
 from rest_framework.authtoken.views import ObtainAuthToken
 from rest_framework.settings import api_settings
 from user.serializers import (
@@ -18,3 +18,12 @@ class CreateUserView(generics.CreateAPIView):
 class CreateTokenView(ObtainAuthToken):
     serializer_class = AuthTokenSerializer
     renderer_classes = api_settings.DEFAULT_RENDERER_CLASSES
+
+
+class ManageUserView(generics.RetrieveAPIView):
+    serializer_class = UserSerializer
+    authentication_classes = authentication.TokenAuthentication
+    permission_classes = permissions.IsAuthenticated
+
+    def get_object(self):
+        return self.request.user
