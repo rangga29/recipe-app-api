b9a452b9033fb8f96e8d5e7d2c1d4a1b32f0b6d9
IT Santa Ursula Bandung <it@santaursula-bdg.sch.id>
2024-03-02 16:53:41 +0700

    Implement Update Tag API

--- app/recipe/views.py
+++ app/recipe/views.py
@@ -32,7 +32,7 @@ class RecipeViewSet(viewsets.ModelViewSet):
         serializer.save(user=self.request.user)
 
 
-class TagViewSet(mixins.ListModelMixin, viewsets.GenericViewSet):
+class TagViewSet(mixins.UpdateModelMixin, mixins.ListModelMixin, viewsets.GenericViewSet):
     """Manage Tag in Database"""
     serializer_class = serializers.TagSerializer
     queryset = Tag.objects.all()
