72b7099bb4ad5c0557cceb7dd580343c83eff12b
IT Santa Ursula Bandung <it@santaursula-bdg.sch.id>
2024-03-02 15:24:38 +0700

    Implement Create Recipe API

--- app/recipe/views.py
+++ app/recipe/views.py
@@ -21,8 +21,12 @@ class RecipeViewSet(viewsets.ModelViewSet):
         return self.queryset.filter(user=self.request.user).order_by('-id')
 
     def get_serializer_class(self):
-        """Return Sertializer Class for Request"""
+        """Return Serializer Class for Request"""
         if self.action == 'list':
             return serializers.RecipeSerializer
 
         return self.serializer_class
+
+    def perform_create(self, serializer):
+        """Create a New Recipe"""
+        serializer.save(user=self.request.user)
