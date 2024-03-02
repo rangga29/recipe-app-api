50060078355df3205fb5dda28f58a0826ebd4a41
IT Santa Ursula Bandung <it@santaursula-bdg.sch.id>
2024-03-02 15:14:21 +0700

    Write Tests for Creating Recipes

--- app/recipe/serializers.py
+++ app/recipe/serializers.py
@@ -8,8 +8,13 @@ from core.models import Recipe
 
 class RecipeSerializer(serializers.ModelSerializer):
     """Serializer for Recipe"""
-
     class Meta:
         model = Recipe
         fields = ['id', 'title', 'time_minutes', 'price', 'link']
         read_only_fields = ['id']
+
+
+class RecipeDetailSerializer(RecipeSerializer):
+    """Serializer for Recipe Detail"""
+    class Meta(RecipeSerializer.Meta):
+        fields = RecipeSerializer.Meta.fields + ['description']
