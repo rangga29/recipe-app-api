5088d501de6d72e1a8b4f9a4f3c04024df36858a
IT Santa Ursula Bandung <it@santaursula-bdg.sch.id>
2024-03-02 14:55:42 +0700

    Implement Recipe Listing API

--- app/recipe/views.py
+++ app/recipe/views.py
@@ -1,3 +1,21 @@
-from django.shortcuts import render
+"""
+Views for Recipe API
+"""
 
-# Create your views here.
+from rest_framework import viewsets
+from rest_framework.authentication import TokenAuthentication
+from rest_framework.permissions import IsAuthenticated
+from core.models import Recipe
+from recipe import serializers
+
+
+class RecipeViewSet(viewsets.ModelViewSet):
+    """View for Manage Recipe API"""
+    serializer_class = serializers.RecipeSerializer
+    queryset = Recipe.objects.all()
+    authentication_classes = [TokenAuthentication]
+    permission_classes = [IsAuthenticated]
+
+    def get_queryset(self):
+        """Retrieve Recipes for Authenticated User"""
+        return self.queryset.filter(user=self.request.user).order_by('-id')
