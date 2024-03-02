feaa8a6c58c98147fafeb77a36ab3a163b46094a
IT Santa Ursula Bandung <it@santaursula-bdg.sch.id>
2024-03-02 14:08:50 +0700

    Write Tests for Recipe Model

--- app/core/tests/test_models.py
+++ app/core/tests/test_models.py
@@ -2,8 +2,10 @@
 Tests for models
 """
 
+from decimal import Decimal
 from django.test import TestCase
 from django.contrib.auth import get_user_model
+from core import models
 
 
 class ModelTests(TestCase):
@@ -43,3 +45,19 @@ class ModelTests(TestCase):
 
         self.assertTrue(user.is_superuser)
         self.assertTrue(user.is_staff)
+
+    def test_create_recipe(self):
+        """Test Creating a Recipe is Successful"""
+        user = get_user_model().objects.create_user(
+            'test@example.com',
+            'testpass123'
+        )
+        recipe = models.Recipe.objects.create(
+            user=user,
+            title='Sample Recipe Name',
+            time_minutes=5,
+            price=Decimal('5,50'),
+            description='Sample Recipe Description',
+        )
+
+        self.assertEqual(str(recipe), recipe.title)
